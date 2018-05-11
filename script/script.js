9ab07f78ea61f9d00f975ed03499c793f0f83c90
Unknown <ocebeki@gmail.com>
2018-05-11 16:41:36 +0100

    PSD TO HTML

---
+++ script/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+    
+    $('.header__toggleButton').click(function(){
+        $('.header__nav').toggleClass('active');
+
+        
+    })
+});      
+
+
+//    $('.navigation__buttonToggle').click(function () {
+//        $('.navigation__toggle').toggleClass('navigation__open');
+//    });
