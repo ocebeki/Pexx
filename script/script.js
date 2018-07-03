4a449b603cfdc082ca4df1d6ede0d216aa78e677
Unknown <ocebeki@gmail.com>
2018-07-03 19:49:13 +0100

    Fixed nav

--- script/script.js
+++ script/script.js
@@ -1,26 +1,29 @@
-$(document).ready(function(){
-	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
 
-	    var target = this.hash;
-	    var $target = $(target);
+        var target = this.hash;
+        var $target = $(target);
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
-    
-    $('.header__toggleButton').click(function(){
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+
+    $('.header__toggleButton').click(function () {
         $('.header__nav').toggleClass('active');
         $('.header__toggleButton span').toggleClass('open');
-
-        
     })
-});      
+    $(window).bind('scroll', function () {
+        if ($(window).scrollTop() > 50) {
+            $('.header__nav').addClass('header__navFixed');
+            
+        }else {
+            $('.header__nav').removeClass('header__navFixed');
+        }
 
+    });
 
-//    $('.navigation__buttonToggle').click(function () {
-//        $('.navigation__toggle').toggleClass('navigation__open');
-//    });
+});
